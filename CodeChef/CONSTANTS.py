6d038c3ffa3d66ef319e7392eac6fe91ecc79434
RAOexe <2020bteit00034@walchandsangli.ac.in>
2022-06-28 23:48:28 +0530

    Add: CONSTANTS.py - Contains required constants

---
+++ CodeChef/CONSTANTS.py
@@ -0,0 +1,5 @@
+WEB_DRIVER_PATH = "C:\Program Files (x86)\chromedriver.exe"
+CodeChefContest = "https://www.codechef.com/contests"
+LeetCodeHeader = "https://leetcode.com"
+LeetCodeGraphQL = "https://leetcode.com/graphql/"
+LeetCodeContest = "https://leetcode.com/_next/data/Ldb_Fcc-UKa_pLAqMwyAp/problemset/all.json"
