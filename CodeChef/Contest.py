4b91ebcd0d8a86ca42b2864c29b217386be305e6
RAOexe <2020bteit00034@walchandsangli.ac.in>
2022-06-29 09:36:22 +0530

    Add: Contest Class

--- CodeChef/Contest.py
+++ CodeChef/Contest.py
@@ -8,5 +8,6 @@ class Contest:
     endTime = ""
     duration = ""
 
+
     def makeContest(self):
         return {"code": self.code, "name": self.name, "startDate": self.startDate, "startTime": self.startTime, "endTime": self.endTime, "duration": self.duration}
